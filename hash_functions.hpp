2624c59a19ea5a041452da7ec889c9a6a1b90021
rafalp2004 <rafalprzybyl17@gmail.com>
2025-06-09 17:52:18 +0200

    commit

--- hash_functions.hpp
+++ hash_functions.hpp
@@ -1,44 +1,86 @@
 #pragma once
-#include <functional>
+
+#include <string>
+#include <cmath>
+#include <type_traits>
 #include <random>
 #include <chrono>
 
+
+// Pomocnicza funkcją hashująca
+class BasicHasher {
+public:
+    // Liczby całkowite (int, long, char itp.)
+    template<typename T>
+    static typename std::enable_if<std::is_integral<T>::value, int>::type
+    hash(const T& key) {
+        return std::abs(static_cast<int64_t>(key));
+    }
+
+    // Liczby zmiennoprzecinkowe (float, double)
+    template<typename T>
+    static typename std::enable_if<std::is_floating_point<T>::value, int>::type
+    hash(const T& key) {
+        return std::abs(static_cast<int64_t>(key * 100000)); 
+    }
+
+    // Ciągi znaków
+    static int hash(const std::string& key) {
+        int hash = 0;
+        for (char c : key) {
+            hash = 31 * hash + static_cast<int>(c);
+        }
+        return std::abs(hash);
+    }
+
+    // Pojedynczy znak
+    static int hash(char c) {
+        return static_cast<int>(c);
+    }
+
+};
+
+
+// MODULO HASH
+
+template<typename K>
 class ModuloHash {
 private:
     int capacity;
+
 public:
     ModuloHash(int cap = 101) : capacity(cap) {}
 
-    template<typename K>
     int operator()(const K& key) const {
-        std::hash<K> hash_fn;
-        return static_cast<int>(hash_fn(key) % capacity);
+        return BasicHasher::hash(key) % capacity;
     }
 };
 
+
+// MULTIPLICATIVE HASH
+template<typename K>
 class MultiplicativeHash {
 private:
-    static constexpr double A = 0.6180339887;  
+    static constexpr double A = 0.6180339887;
     int capacity;
 
 public:
     explicit MultiplicativeHash(int cap = 101) : capacity(cap) {}
 
-    template<typename K>
     int operator()(const K& key) const {
-        std::hash<K> hash_fn;
-        double hash_val = static_cast<double>(hash_fn(key));
-
-        double intpart;
-        double fractional = std::modf(hash_val * A, &intpart);  
-
+        double fractional = std::fmod(BasicHasher::hash(key) * A, 1.0);
         return static_cast<int>(capacity * fractional);
     }
 };
 
+
+// UNIVERSAL HASH
+
+template<typename K>
 class UniversalHash {
 private:
     int a, b, p, capacity;
+
 public:
     UniversalHash(int cap = 101)
         : capacity(cap), p(10000019) {
@@ -49,9 +91,7 @@ public:
         b = dist(gen);
     }
 
-    template<typename K>
     int operator()(const K& key) const {
-        std::hash<K> hash_fn;
-        return static_cast<int>(((int64_t(a) * hash_fn(key) + b) % p) % capacity);
+        return static_cast<int>(((int64_t(a) * BasicHasher::hash(key) + b) % p) % capacity);
     }
 };
